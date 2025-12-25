b575550b665de57a88cf4f7ec2df9c6eb53a5bc3
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-25 19:30:15 +0200

    adding constants file

---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Kaufra";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_APP_DESCRIPTION ||
+  "A modern E-commerce platform build with Next.js";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
