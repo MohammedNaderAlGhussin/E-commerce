d261ed44b1e868419ac2b0bfda2733c583304bda
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-25 20:38:04 +0200

    enhancing footer component

--- components/layout/Footer/Footer.tsx
+++ components/layout/Footer/Footer.tsx
@@ -1,5 +1,28 @@
+import { APP_NAME } from "@/lib/constants";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="border-t">
+      <div className="p-5 flex-center flex-col">
+        <div className="flex my-2">
+          <p className="mr-2">
+            made with ❤️ by
+            <a
+              className="font-bold hover:text-primary transition-primary ml-1"
+              href="https://github.com/MohammedNaderAlGhussin"
+              target="_blank"
+            >
+              Mohammednader AlGhussin
+            </a>
+          </p>
+        </div>
+        <div>
+          © {currentYear} {APP_NAME}. All Rights Reserved.
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
