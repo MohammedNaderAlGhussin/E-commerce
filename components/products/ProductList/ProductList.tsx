659344a169af27c612bdc74d963aa6ddeccad8ef
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-26 18:08:53 +0200

    create the productList component

---
+++ components/products/ProductList/ProductList.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ProductListProps } from "./ProductList.types";
+import { Product } from "../Products.types";
+import ProductCard from "../ProductCard/ProductCard";
+
+const ProductList = ({ data, title, limit }: ProductListProps) => {
+  const limitedData = limit ? data.slice(0, limit) : data;
+
+  return (
+    <div className="my-10">
+      <h2 className="h2-bold mb-4">{title}</h2>
+      {data.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {limitedData.map((product: Product) => (
+            <ProductCard key={product.slug} product={product} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No products found</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
