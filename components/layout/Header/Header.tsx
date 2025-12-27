526af3777d7450ef0743851a36902e9b4076fcd5
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-27 16:22:00 +0200

    rendering logo inside header

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,26 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
-import Logo from "@/public/images/logo.svg";
-import { APP_NAME } from "@/lib/constants";
 import HeaderMenu from "./HeaderMenu";
+import Logo from "./Logo";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/" className="flex-start">
-            <Image
-              src={Logo}
-              alt={`${APP_NAME} logo`}
-              width={48}
-              height={48}
-              priority
-            />
-            <span className="hidden lg:block font-bold text-2xl ml-3">
-              {APP_NAME}
-            </span>
-          </Link>
+          <Logo />
         </div>
         <HeaderMenu />
       </div>
