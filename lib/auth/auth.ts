b3ef56459394f559bfac9c248becb2ccff9b7e05
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-29 02:39:23 +0200

    adding true auth to auth.ts file

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -7,6 +7,7 @@ import prisma from "../prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  trustHost: true,
   pages: {
     signIn: "/sign-in",
     error: "/sign-in",
