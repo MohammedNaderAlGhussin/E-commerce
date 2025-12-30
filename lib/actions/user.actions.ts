42ad463690f48630d55846aacc720be7f1f80482
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-31 01:08:02 +0200

    adding update user address function

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -94,3 +94,30 @@ export async function getUserById(userID: string) {
   if (!user) throw new Error("User was not found!");
   return user;
 }
+
+// Update the user's address
+export async function updateUserAddress(data: ShippingAddress) {
+  try {
+    const session = await auth();
+
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    });
+
+    if (!currentUser) throw new Error("User not found");
+
+    const address = shippingAddressSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { address },
+    });
+
+    return {
+      success: true,
+      message: "User updated successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
