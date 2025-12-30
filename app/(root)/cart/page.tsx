433f6651a49de1dd387370a16feae2211b763b76
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-30 08:33:44 +0200

    creating cart route and page

---
+++ app/(root)/cart/page.tsx
@@ -0,0 +1,18 @@
+import CartTable from "./CartTable";
+import { getMyCart } from "@/lib/actions/cart.actions";
+
+export const metadata = {
+  title: "Shopping Cart",
+};
+
+const CartPage = async () => {
+  const cart = await getMyCart();
+
+  return (
+    <>
+      <CartTable cart={cart} />
+    </>
+  );
+};
+
+export default CartPage;
