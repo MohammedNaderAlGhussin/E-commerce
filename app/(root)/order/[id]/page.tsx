07270c08ae72e2d9dc52b4e8f7786a37aee40dbd
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-31 08:06:21 +0200

    creating order/id route and page

---
+++ app/(root)/order/[id]/page.tsx
@@ -0,0 +1,35 @@
+import { Metadata } from "next";
+import { getOrderById } from "@/lib/actions/order.actions";
+import { notFound } from "next/navigation";
+import { ShippingAddress } from "@/types/shipping-adress";
+import OrderDetailsTable from "./OrderDetailsTable";
+
+export const metadata: Metadata = {
+  title: "Order Details",
+};
+
+type OrderDetailsPageProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+const OrderDetailsPage = async (props: OrderDetailsPageProps) => {
+  const { id } = await props.params;
+
+  const order = await getOrderById(id);
+  if (!order) notFound();
+
+
+
+  return (
+    <OrderDetailsTable
+      order={{
+        ...order,
+        shippingAddress: order.shippingAddress as ShippingAddress,
+      }}
+    />
+  );
+};
+
+export default OrderDetailsPage;
