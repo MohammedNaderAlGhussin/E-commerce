712cdff70e8305c219131ad2c89dd5806486f9f8
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2026-01-02 14:33:52 +0200

    protecting route while user is not logged in

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -1,8 +1,9 @@
 import { Metadata } from "next";
 import { getOrderById } from "@/lib/actions/order.actions";
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import { ShippingAddress } from "@/types/shipping-adress";
 import OrderDetailsTable from "./OrderDetailsTable";
+import { auth } from "@/lib/auth/auth";
 
 export const metadata: Metadata = {
   title: "Order Details",
@@ -16,6 +17,12 @@ type OrderDetailsPageProps = {
 
 const OrderDetailsPage = async (props: OrderDetailsPageProps) => {
   const { id } = await props.params;
+
+  const session = await auth();
+
+  if (!session?.user) {
+    redirect("/sign-in");
+  }
 
   const order = await getOrderById(id);
   if (!order) notFound();
