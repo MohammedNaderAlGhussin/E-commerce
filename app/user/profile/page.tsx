dd80e439eb8c98f947caafd23c5e358413a839eb
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2026-01-02 14:31:39 +0200

    creating a /user/profile path and page

---
+++ app/user/profile/page.tsx
@@ -0,0 +1,28 @@
+import { auth } from "@/lib/auth/auth";
+import { Metadata } from "next";
+import { SessionProvider } from "next-auth/react";
+import { redirect } from "next/navigation";
+// import ProfileForm from "./profile-form";
+
+export const metadata: Metadata = {
+  title: "Customer Profile",
+};
+
+const Profile = async () => {
+  const session = await auth();
+
+  if (!session?.user) {
+    redirect("/sign-in");
+    }
+    
+  return (
+    <SessionProvider session={session}>
+      <div className="max-w-md mx-auto space-y-4">
+        <h2 className="h2-bold">Profile</h2>
+        {/* <ProfileForm /> */}
+      </div>
+    </SessionProvider>
+  );
+};
+
+export default Profile;
