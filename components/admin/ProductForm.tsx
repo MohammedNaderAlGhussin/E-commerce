561e239a824db7568c17cda7a7d88df56cc1522b
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2026-01-08 03:56:51 +0200

    creating upload image input

--- components/admin/ProductForm.tsx
+++ components/admin/ProductForm.tsx
@@ -26,6 +26,9 @@ import z from "zod";
 import { Textarea } from "../ui/textarea";
 import { toast } from "sonner";
 import { createProduct, updateProduct } from "@/lib/actions/product.actions";
+import { Card, CardContent } from "../ui/card";
+import Image from "next/image";
+import { UploadButton } from "@/lib/validators/uploadthing";
 
 type ProductFormProps = {
   type: "Create" | "Update";
@@ -78,6 +81,9 @@ const ProductForm = ({ type, product, productId }: ProductFormProps) => {
       }
     }
   };
+
+  const images = form.watch("images");
+
   return (
     <Form {...form}>
       <form
@@ -224,6 +230,43 @@ const ProductForm = ({ type, product, productId }: ProductFormProps) => {
         </div>
         <div className="upload-field flex flex-col md:flex-row gap-5">
           {/* Image */}
+          <FormField
+            control={form.control}
+            name="images"
+            render={() => (
+              <FormItem className="w-full">
+                <FormLabel className="mb-1">Images</FormLabel>
+                <Card>
+                  <CardContent className="space-y-2 mt-2 min-h-48">
+                    <div className="flex-start space-x-2">
+                      {images.map((image: string) => (
+                        <Image
+                          key={image}
+                          src={image}
+                          alt="product image"
+                          className="w-20 h-20 object-cover object-center rounded-sm"
+                          width={100}
+                          height={100}
+                        />
+                      ))}
+                      <FormControl>
+                        <UploadButton
+                          endpoint="imageUploader"
+                          onClientUploadComplete={(res: { url: string }[]) => {
+                            form.setValue("images", [...images, res[0].url]);
+                          }}
+                          onUploadError={(error: Error) => {
+                            toast.error(`ERROR! ${error.message}`);
+                          }}
+                        />
+                      </FormControl>
+                    </div>
+                  </CardContent>
+                </Card>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
         <div className="upload-field">{/* Is Featured */}</div>
         <div>
