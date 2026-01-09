c1834d06e02d71cb7710597416b4e76044f14342
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2026-01-09 21:22:09 +0200

    creating admin search component

---
+++ components/admin/AdminSearch.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { usePathname, useSearchParams } from "next/navigation";
+import { Input } from "../ui/input";
+
+const AdminSearch = () => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const formActionUrl = pathname.includes("/admin/orders")
+    ? "/admin/orders"
+    : pathname.includes("/admin/users")
+    ? "/admin/users"
+    : "/admin/products";
+
+  const queryValue = searchParams.get("query") || "";
+
+  return (
+    <form action={formActionUrl} method="GET">
+      <Input
+        type="search"
+        placeholder="Search..."
+        name="query"
+        defaultValue={queryValue}
+        className="md:w-25 lg:w-75"
+      />
+      <button className="sr-only" type="submit">
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default AdminSearch;
