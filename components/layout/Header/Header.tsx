eb32aa47e5b895a4ecf5ec73c8f7877357de75f1
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2026-01-09 23:15:53 +0200

    adding drawer comp to main header

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -2,13 +2,15 @@ import { APP_NAME } from "@/lib/constants";
 import HeaderMenu from "./HeaderMenu";
 import Logo from "./Logo";
 import Link from "next/link";
+import CategoryDrawer from "./CategoryDrawer";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/" className="flex-start">
+          <CategoryDrawer />
+          <Link href="/" className="flex-start ml-4">
             <Logo width={60} height={50} />
             <span className="hidden lg:block font-bold text-2xl ml-3">
               {APP_NAME}
