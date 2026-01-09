b1d7b8f60157534a187adbf0d7d6c9f02504e12e
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2026-01-09 21:23:14 +0200

    modifying spacing

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -33,7 +33,7 @@ const AdminOrdersPage = async (props: {
 
   return (
     <div className="space-y-2">
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-3 mb-5">
         <h1 className="h2-bold">Orders</h1>
         {searchText && (
           <div>
