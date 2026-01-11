45e0d94abeade0698640b38507b5cb2e74f3b1e9
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2026-01-11 20:14:24 +0200

    creating stripe client sevrect and passing it to order deatial table

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -4,6 +4,7 @@ import { notFound, redirect } from "next/navigation";
 import { ShippingAddress } from "@/types/shipping-adress";
 import OrderDetailsTable from "./OrderDetailsTable";
 import { auth } from "@/lib/auth/auth";
+import Stripe from "stripe";
 
 export const metadata: Metadata = {
   title: "Order Details",
@@ -27,6 +28,21 @@ const OrderDetailsPage = async (props: OrderDetailsPageProps) => {
   const order = await getOrderById(id);
   if (!order) notFound();
 
+  let client_secret = null;
+
+  // Check if is not paid and using stripe
+  if (order.paymentMethod === "Stripe" && !order.isPaid) {
+    // Init stripe instance
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+    // Create payment intent
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(Number(order.totalPrice) * 100),
+      currency: "USD",
+      metadata: { orderId: order.id },
+    });
+    client_secret = paymentIntent.client_secret;
+  }
+
   return (
     <OrderDetailsTable
       order={{
@@ -34,6 +50,7 @@ const OrderDetailsPage = async (props: OrderDetailsPageProps) => {
         shippingAddress: order.shippingAddress as ShippingAddress,
       }}
       paypalClientId={process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!}
+      stripeClientSecret={client_secret}
       isAdmin={session?.user?.role === "admin" || false}
     />
   );
