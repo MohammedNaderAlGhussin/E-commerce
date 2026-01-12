0ab5829e3845f32682b75acff59ef1c9e6117d72
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2026-01-12 23:08:43 +0200

    handling empty cart appearance

--- app/(root)/cart/CartTable.tsx
+++ app/(root)/cart/CartTable.tsx
@@ -12,7 +12,7 @@ import {
   TableCell,
 } from "@/components/ui/table";
 import Image from "next/image";
-import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
+import { ArrowRight, Loader, Minus, Plus, ShoppingCart } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 import { toast } from "sonner";
@@ -20,6 +20,14 @@ import { Card, CardContent } from "@/components/ui/card";
 import { formatCurrency } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 import { useWindowSize } from "@/hooks/useWindowSize";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
   const router = useRouter();
@@ -30,84 +38,83 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
 
   return (
     <>
-      <h1 className="py-4 h2-bold">Shopping Cart</h1>
       {!cart || cart.items.length === 0 ? (
-        <div>
-          Cart is empty.{" "}
-          <Button asChild variant="default">
-            <Link href="/">Go Shopping</Link>
-          </Button>
-        </div>
+        <EmptyCart />
       ) : (
-        <div className="grid md:grid-cols-4 md:gap-5">
-          <div className="overflow-x-auto md:col-span-3">
-            {" "}
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Item</TableHead>
-                  <TableHead className="text-center">Quantity</TableHead>
-                  <TableHead className="text-right">Price</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {cart.items.map((item) => (
-                  <TableRow key={item.slug}>
-                    <TableCell>
-                      <Link
-                        href={`/product/${item.slug}`}
-                        className="flex items-center"
-                      >
-                        <Image
-                          src={item.image}
-                          alt={item.name}
-                          width={50}
-                          height={50}
-                        />
-                        <span className="px-2">
-                          {screenWidth && screenWidth < 550
-                            ? `${item.name.slice(0, 18)}...`
-                            : item.name}
-                        </span>
-                      </Link>
-                    </TableCell>
-                    <TableCell className="flex-center gap-2 mt-3">
-                      <RemoveButton item={item} />
-                      <span>{item.qty}</span>
-                      <AddButton item={item} />
-                    </TableCell>
-                    <TableCell className="text-right">€{item.price}</TableCell>
+        <>
+          <h1 className="py-4 h2-bold">Shopping Cart</h1>
+          <div className="grid md:grid-cols-4 md:gap-5">
+            <div className="overflow-x-auto md:col-span-3">
+              {" "}
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Item</TableHead>
+                    <TableHead className="text-center">Quantity</TableHead>
+                    <TableHead className="text-right">Price</TableHead>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
+                </TableHeader>
+                <TableBody>
+                  {cart.items.map((item) => (
+                    <TableRow key={item.slug}>
+                      <TableCell>
+                        <Link
+                          href={`/product/${item.slug}`}
+                          className="flex items-center"
+                        >
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={50}
+                            height={50}
+                          />
+                          <span className="px-2">
+                            {screenWidth && screenWidth < 550
+                              ? `${item.name.slice(0, 18)}...`
+                              : item.name}
+                          </span>
+                        </Link>
+                      </TableCell>
+                      <TableCell className="flex-center gap-2 mt-3">
+                        <RemoveButton item={item} />
+                        <span>{item.qty}</span>
+                        <AddButton item={item} />
+                      </TableCell>
+                      <TableCell className="text-right">
+                        €{item.price}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
+            <Card className="mt-5 lg:mt-0">
+              <CardContent className="px-4 py-2 flex flex-col flex-1 justify-between">
+                <div className="pb-3 text-xl text-center">
+                  Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):
+                  <span className="font-bold">
+                    {" "}
+                    {formatCurrency(cart.itemsPrice)}
+                  </span>
+                </div>
+                <Button
+                  className="w-full"
+                  disabled={isPending}
+                  onClick={() =>
+                    startTransition(() => router.push("/shipping-address"))
+                  }
+                >
+                  {isPending ? (
+                    <Loader className="w-4 h-4 animate-spin" />
+                  ) : (
+                    <ArrowRight className="w-4 h-4" />
+                  )}{" "}
+                  Proceed to Checkout
+                </Button>
+              </CardContent>
+            </Card>
           </div>
-          <Card className="mt-5 lg:mt-0">
-            <CardContent className="px-4 py-2 flex flex-col flex-1 justify-between">
-              <div className="pb-3 text-xl text-center">
-                Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):
-                <span className="font-bold">
-                  {" "}
-                  {formatCurrency(cart.itemsPrice)}
-                </span>
-              </div>
-              <Button
-                className="w-full"
-                disabled={isPending}
-                onClick={() =>
-                  startTransition(() => router.push("/shipping-address"))
-                }
-              >
-                {isPending ? (
-                  <Loader className="w-4 h-4 animate-spin" />
-                ) : (
-                  <ArrowRight className="w-4 h-4" />
-                )}{" "}
-                Proceed to Checkout
-              </Button>
-            </CardContent>
-          </Card>
-        </div>
+        </>
       )}
     </>
   );
@@ -165,3 +172,27 @@ function AddButton({ item }: { item: CartItem }) {
 }
 
 export default CartTable;
+
+export function EmptyCart() {
+  return (
+    <Empty>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <ShoppingCart />
+        </EmptyMedia>
+        <EmptyTitle>Cart Is Empty !</EmptyTitle>
+        <EmptyDescription>
+          You haven&apos;t selected any item yet. Get started by selecting your
+          first product.
+        </EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent>
+        <div className="flex gap-2">
+          <Button asChild variant="default">
+            <Link href="/">Go Shopping</Link>
+          </Button>
+        </div>
+      </EmptyContent>
+    </Empty>
+  );
+}
